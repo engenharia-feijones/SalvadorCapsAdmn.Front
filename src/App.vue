25143be9b34ed9021d0654066e97c336f385f18a
Ismael Souza <souz4ismael@gmail.com>
2021-05-13 19:10:35 -0300

    Separando Forms

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,25 @@
 <template>
   <v-app>
-    <Marca/>
+    <v-container>
+      <v-row cols="12">
+        <v-col cols="12" md="2" class="ma-0"
+          ><v-btn block :to="{ name: 'Marca' }">1. Marca</v-btn></v-col
+        >
+        <v-col cols="12" md="2"
+          ><v-btn block :to="{ name: 'Categoria' }">2. Categoria</v-btn></v-col
+        >
+        <v-col cols="12" md="2"><v-btn block>3. Produtos</v-btn></v-col>
+      </v-row>
+    </v-container>
+    <v-main>
+      <router-view></router-view>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-
-import Marca from './components/Marca/Marca.vue'
-
 export default {
-  name: 'App',
-
-  components: {
-
-    Marca,
-  },
+  name: "App",
 
   data: () => ({
     //
