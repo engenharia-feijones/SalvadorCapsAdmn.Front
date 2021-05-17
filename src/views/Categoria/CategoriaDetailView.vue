8e49698aec11135fc457ea90f4a0da6d62b0b332
Ismael Souza <souz4ismael@gmail.com>
2021-05-16 22:17:49 -0300

    Created Delete Components

---
+++ src/views/Categoria/CategoriaDetailView.vue
@@ -0,0 +1,13 @@
+<template>  
+    <CategoriaDetail />
+</template>
+
+<script>
+import CategoriaDetail from '@/components/Categoria/CategoriaDetail/CategoriaDetail.vue'
+export default {
+    name: "CategoriaView",
+    components: {
+        CategoriaDetail
+    }
+}
+</script>
