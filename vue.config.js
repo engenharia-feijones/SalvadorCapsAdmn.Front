3bf5575d0f87542de772219f5a28a2919b5d64ca
Pedro <puedroreis@gmail.com>
2021-06-04 01:43:04 -0300

    add fav icon

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  chainWebpack: config => {
+    config.module.rules.delete('eslint');
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "ADM Salvador Caps";
+            return args;
+        })
+},
   transpileDependencies: [
     'vuetify'
   ]
